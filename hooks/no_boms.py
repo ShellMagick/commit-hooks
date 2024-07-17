891139c59fb92b35c7384a231ba2464b3144b724
Dániel Kovács <daniel@kovacs.xyz>
2024-07-17 11:42:26 +0200

    Print full path from no-boms

--- hooks/no_boms.py
+++ hooks/no_boms.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import argparse
 from collections.abc import Sequence
-from os.path import basename
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -21,19 +20,19 @@ def main(argv: Sequence[str] | None = None) -> int:
         # Cf. https://www.unicode.org/faq/utf_bom.html#BOM
         if candidate == b'\xff\xfe\x00\x00':
             result *= 13
-            print(f'{basename(filename)}: has a UTF-32 BOM (LE)')
+            print(f'{filename}: has a UTF-32 BOM (LE)')
         elif candidate == b'\x00\x00\xfe\xff':
             result *= 11
-            print(f'{basename(filename)}: has a UTF-32 BOM (BE)')
+            print(f'{filename}: has a UTF-32 BOM (BE)')
         elif candidate[:2] == b'\xff\xfe':
             result *= 7
-            print(f'{basename(filename)}: has a UTF-16 BOM (LE)')
+            print(f'{filename}: has a UTF-16 BOM (LE)')
         elif candidate[:2] == b'\xfe\xff':
             result *= 5
-            print(f'{basename(filename)}: has a UTF-16 BOM (BE)')
+            print(f'{filename}: has a UTF-16 BOM (BE)')
         elif candidate[:3] == b'\xef\xbb\xbf':
             result *= 3
-            print(f'{basename(filename)}: has a UTF-8 BOM')
+            print(f'{filename}: has a UTF-8 BOM')
 
     return result % 256 if result != 1 else 0
 
